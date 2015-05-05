f14fc7af7edbed40d40095709097c804fc8642d1
jogle <jiugea@gmail.com>
2015-05-05 18:02:00 +0800

    workerman 修改

--- Applications/SimpleChat/start_websocket_server.php
+++ Applications/SimpleChat/start_websocket_server.php
@@ -1,6 +1,7 @@
 <?php 
 use \Workerman\Worker;
 use \Workerman\Autoloader;
+use \GatewayWorker\Lib\Db;
 
 // autoload
 require_once __DIR__ . '/../../Workerman/Autoloader.php';
@@ -20,24 +21,72 @@ $ws_server->onConnect = function($connection)
     $connection->onWebSocketConnect = function($connection)
     {
         $data = array(
-                'type' => 'login',
+                'type' => 'LGN',
                 'time' => date('Y-m-d H:i:s'),
                 // @see http://doc3.workerman.net/worker-development/id.html
-                'from_id' => $connection->id,
+                'id' => $connection->id,
+                'headpic' => '/Public/Image/avatar.jpg',
+                'name' => '用户'.$connection->id
         );
         broad_cast(json_encode($data));
     };
 };
 
 // @see http://doc3.workerman.net/worker-development/on-message.html
-$ws_server->onMessage = function($connection, $data)use($ws_server)
+$ws_server->onMessage = function($connection, $content)use($ws_server)
 {
+    $scontent=json_decode($content,true);
+    $time = date('Y-m-d H:i:s');
+    $headpic='/Public/Image/avatar.jpg';
+    $name='';
+    if($scontent&&isset($scontent['bookid'])&&$scontent['chatid']&&$scontent['code']){
+        $id=$scontent['chatid'];
+
+        if(verify_code($scontent['chatid'],$scontent['code'])){
+            $type='MSG';
+            $tcontent=isset($scontent['content'])?$scontent['content']:'';
+            if(isset($_SESSION['user_'.$scontent['chatid']])){
+                $userinfo=$_SESSION['user_'.$scontent['chatid']];
+            }
+            else{
+                $userinfo=Db::instance('db1')->select('nickname,username,gender,photo')->from('tbl_user')->where('userid='.$scontent['chatid'])->row();
+                var_dump($userinfo);
+
+                if($userinfo){
+                    $_SESSION['user_'.$scontent['chatid']]=$userinfo;
+                }
+                else{
+                    $type='SYS';
+                    $tcontent='账号错误，消息：“'.$scontent['content'].'”，发送失败';
+                }
+
+            }
+            if($userinfo){
+                $headpic=$userinfo['photo']!=''?$userinfo['photo']:'/Public/Image/avatar3.jpg';
+                $name=$userinfo['nickname']!=''?$userinfo['nickname']:$userinfo['username'];
+            }
+
+        }
+        else{
+            $type='SYS';
+            $tcontent='系统验证失败，消息：“'.$scontent['content'].'”，发送失败';
+        }
+
+    }
+    else{
+        $type='SYS';
+        $tcontent='参数错误，消息：“'.$scontent['content'].'”，发送失败';
+        $id=-1;
+
+    }
     $data = array(
-        'type' => 'say',
-        'content' => $data,
-        'time' => date('Y-m-d H:i:s'),
+        'type' => $type,
+        'content' => $tcontent,
+        'time' => $time,
         // @see http://doc3.workerman.net/worker-development/id.html
-        'from_id' => $connection->id,
+        'id' =>$id,
+        'headpic' => $headpic,
+        'name' => $name
     );
     broad_cast(json_encode($data));
 };
@@ -46,10 +95,12 @@ $ws_server->onMessage = function($connection, $data)use($ws_server)
 $ws_server->onClose = function($connection)
 {
     $data = array(
-                'type' => 'logout',
+                'type' => 'LGT',
                 'time' => date('Y-m-d H:i:s'),
                 // @see http://doc3.workerman.net/worker-development/id.html
-                'from_id' => $connection->id,
+                'id' => $connection->id,
+                'headpic' => '/Public/Image/avatar.jpg',
+                'name' => '用户'.$connection->id
         );
         broad_cast(json_encode($data));
 };
@@ -67,6 +118,15 @@ function broad_cast($msg)
     {
         // @see http://doc3.workerman.net/worker-development/send.html
         $connection->send($msg);
+    }
+}
+
+function verify_code($id,$code){
+    if(substr(md5('@#'.$id.'*&^'),3,25)==$code){
+        return true;
+    }
+    else{
+        return false;
     }
 }
 
